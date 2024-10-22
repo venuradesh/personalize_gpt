293901fc591ad210ce4d5a4b3cf7576f8aad87e1
venurawarnasooriya <venura@ashogu.com>
2024-10-22 23:30:29 +0530

    nav bar moved to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { ThemeService } from "./services/theme.service";
+import { NavBarComponent } from "./core/layout/nav-bar/nav-bar.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavBarComponent],
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
   constructor() {}
