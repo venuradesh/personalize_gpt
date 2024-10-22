ddd4269eddd05810e8a360177b750c9bec85785c
venurawarnasooriya <venura@ashogu.com>
2024-10-22 23:31:14 +0530

    register page implemented

---
+++ src/app/pages/register-page/register-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pgpt-register-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.scss'
+})
+export class RegisterPageComponent {
+
+}
