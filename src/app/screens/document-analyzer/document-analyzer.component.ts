d58ea88cbf927b3503c89e4844e786e8723c45a4
venurawarnasooriya <venura@ashogu.com>
2025-01-20 10:42:05 +0530

    cleanups

--- src/app/screens/document-analyzer/document-analyzer.component.ts
+++ src/app/screens/document-analyzer/document-analyzer.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { ButtonComponent } from "../../core/components/button/button.component";
 import { FormInputComponent } from "../../core/components/form-input/form-input.component";
-import { ChatWelcomeComponent } from "../../core/components/chat-welcome/chat-welcome.component";
 import { PgptTranslatePipe } from "../../core/Pipes/pgpt-translate.pipe";
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { BehaviorSubject } from "rxjs";
@@ -10,7 +9,7 @@ import { ChatDataSource } from "../../core/models/chat-models";
 import { ChatTileComponent } from "../../core/components/chat-tile/chat-tile.component";
 import { DocPreviewComponent } from "../../core/components/doc-preview/doc-preview.component";
 import { TastrService } from "../../services/tastr.service";
-import { ChatbotState, DocAnalyzerService } from "../../services/doc-analyzer.service";
+import { DocAnalyzerService } from "../../services/doc-analyzer.service";
 import { FormValidator } from "../../core/helpers/validators/form-validators";
 
 @Component({
