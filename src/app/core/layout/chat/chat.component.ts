844984e0228fcf6980837e70b084e42e5e8d15b6
venurawarnasooriya <venura@ashogu.com>
2025-01-20 13:36:06 +0530

    chat interaction basics are done

--- src/app/core/layout/chat/chat.component.ts
+++ src/app/core/layout/chat/chat.component.ts
@@ -5,11 +5,13 @@ import { PgptTranslatePipe } from "../../Pipes/pgpt-translate.pipe";
 import { FormControl, FormGroup, FormsModule } from "@angular/forms";
 import { FormValidator } from "../../helpers/validators/form-validators";
 import { ChatWelcomeComponent } from "../../components/chat-welcome/chat-welcome.component";
-import { ChatDataSource } from "../../models/chat-models";
-import { BehaviorSubject, Subject } from "rxjs";
+import { ChatDataSource, UserRole } from "../../models/chat-models";
+import { BehaviorSubject, Subject, take } from "rxjs";
 import { ChatTileComponent } from "../../components/chat-tile/chat-tile.component";
 import { NavigationService } from "../../../services/navigation.service";
 import { UserModel } from "../../models/user_models";
+import { ChatService } from "../../../services/chat.service";
+import { ApiSource, ErrorSource } from "../../models/api_models";
 
 @Component({
   selector: "pgpt-chat",
@@ -22,23 +24,63 @@ export class ChatComponent implements OnInit, OnDestroy {
   @Input()
   user: UserModel | undefined = undefined;
 
-  prompt = new FormControl("", [FormValidator.requiredValidator("Please Enter your Prompts")]);
+  prompt = new FormControl("", [FormValidator.requiredValidator("Please Enter your Prompt")]);
 
   public chatSource$ = new BehaviorSubject<ChatDataSource[]>([]);
-  chatState: FormGroup;
 
   private destroy$ = new Subject<void>();
 
-  constructor(private navigationService: NavigationService) {
-    this.chatState = new FormGroup({
-      prompt: this.prompt,
-    });
+  constructor(private chatService: ChatService) {}
+
+  public ngOnInit(): void {
+    this._loadSessionChat();
   }
 
-  public ngOnInit(): void {}
-
   public onSubmitClck(): void {
-    console.log("submitted");
+    this.prompt.markAsDirty();
+    this.prompt.updateValueAndValidity();
+
+    if (this.prompt.valid && this.prompt.value) {
+      this._addMessageToArray(this.prompt.value, "user", new Date().toString());
+      this._setMessage(this.prompt.value);
+      this.prompt.reset();
+    }
+  }
+
+  private _loadSessionChat(): void {
+    this.chatService
+      .loadSessionChat()
+      .pipe(take(1))
+      .subscribe({
+        next: (value: ApiSource) => {
+          const data: [] = value.data;
+          data.map((chat) => {
+            this._addMessageToArray(chat["content"], chat["role"] as UserRole, chat["timestamp"]);
+          });
+        },
+        error: (err: ErrorSource) => {
+          console.log(err);
+        },
+      });
+  }
+
+  private _setMessage(userInput: string): void {
+    this.chatService
+      .getPersonalizeGPTResponse(userInput)
+      .pipe(take(1))
+      .subscribe({
+        next: (value: ApiSource) => {
+          const data = value.data;
+          this._addMessageToArray(data.response, "assistant", data.created);
+        },
+        error: (err: ErrorSource) => {
+          console.log(err);
+        },
+      });
+  }
+
+  private _addMessageToArray(message: string, role: UserRole, created: string) {
+    this.chatSource$.next([...this.chatSource$.value, { role: role, content: message, created: new Date(created) }]);
   }
 
   public ngOnDestroy(): void {
---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { environemnt } from "../../environment";
+import { ClientService } from "./client.service";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ChatService {
+  private readonly API_URL = environemnt.API_URL;
+
+  constructor(private clientService: ClientService) {}
+
+  getPersonalizeGPTResponse(userInput: string): Observable<any> {
+    const url = `${this.API_URL}/chat/query`;
+    return this.clientService.post(url, { message: userInput });
+  }
+
+  loadSessionChat(): Observable<any> {
+    const url = `${this.API_URL}/chat/load-session-chat`;
+    return this.clientService.get(url);
+  }
+}
