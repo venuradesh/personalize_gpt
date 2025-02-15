87d027aa27726e7f97b7f17f7361ad21fd6646cd
venurawarnasooriya <venura@ashogu.com>
2025-02-15 19:45:15 +0530

    forgot-password functinality done

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,4 +26,8 @@ export class AuthService {
     const token = this.cookies.get("access_token");
     return !!token;
   }
+
+  public forgotPassword(email: string): Observable<any> {
+    return this.clientService.post(`${this.API_URL}/auth/forgot-password`, { email });
+  }
 }
